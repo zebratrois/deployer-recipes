c1e3931adaeabe08d638b050f43d6d8714337dfc
Zebratrois <zebratrois@gmail.com>
2019-05-16 11:26:13 +0200

    Adding 'save_git_hash' recipe to store current git commit hash in file(s) (to be used as cache buster on assets for example)

---
+++ recipes/save_git_hash.php
@@ -0,0 +1,33 @@
+<?php
+namespace Deployer;
+
+set('git_commit_hash', function() {
+    $git = get('bin/git');
+    cd('{{release_path}}');
+    $hash = run("$git log --pretty=format:'%h' -n 1");
+    return $hash;
+});
+
+set('git_hash_files', [
+    /*
+    [
+        'files' => 'config/*.xml', // source files glob
+        'search' => '<hash></hash>', // string to replace by commit hash
+        'replace' => '<hash>GITHASH</hash>', // replacement string where GITHASH is the commit hash
+    ],
+    */
+]);
+
+desc('Get git commit hash');
+task('save_git_hash', function () {
+    cd('{{release_path}}');
+    $hash = get('git_commit_hash');
+    $files = get('git_hash_files');
+    foreach ($files as $file) {
+        foreach (explode(PHP_EOL, run('ls ' . $file['files'])) as $path) {
+            $cmd = 'sed -e ' . escapeshellarg('s|' . $file['search'] . '|' . str_replace('GITHASH', $hash, $file['replace']) . '|') . ' <' . $path . ' >' . $path . '.tmp';
+            $cmd .= '; mv ' . $path . '.tmp ' . $path;
+            run($cmd);
+        }
+    }
+});

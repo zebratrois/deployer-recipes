a53d13d69774aae9fda88d0ce94af51bb3ae7c37
Zebratrois <zebratrois@gmail.com>
2022-04-11 21:50:06 +0200

    fix using yarn:build in before() before declaring it

--- recipes/yarn.php
+++ recipes/yarn.php
@@ -3,10 +3,10 @@ namespace Deployer;
 
 require 'contrib/yarn.php';
 
-before('yarn:build', 'yarn:install');
-
 desc('Build assets');
 task('yarn:build', function () {
     $target = 'prod' == currentHost()->get('stage') ? 'build' : 'dev';
     run("cd {{release_or_current_path}} && {{bin/yarn}} " . $target);
 });
+
+before('yarn:build', 'yarn:install');
